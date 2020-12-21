8b8e3cd427b4a6445afad3c1da03751a19921f55
John López <johnalbh@gmail.com>
2020-12-21 10:45:59 +0100

    [Test1] - Creación proyecto UnitTest.

---
+++ FirstTestProject.Tests/TaxServiceShould.cs
@@ -0,0 +1,22 @@
+﻿using FirstTestProject.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FirstTestProject.Tests
+{
+    [TestClass]
+    public class TaxServiceShould
+    {
+        [TestMethod]
+        public void Return1000IfGrossSalaaryIs9500()
+        {
+            var taxService = new TaxService();
+            Assert.AreEqual(1000,taxService.GetTax(9500));
+        }
+        [TestMethod]
+        public void Return2000IfGrossSalaaryIs19500()
+        {
+            var taxService = new TaxService();
+            Assert.AreEqual(2000, taxService.GetTax(19500));
+        }
+    }
+}
